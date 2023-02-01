df9345b2216c202c939faceed45aa6081e8eed1b
Makism.Armenskiy <maxim.arm8@mail.ru>
2023-02-01 21:20:49 +0300

    add files

---
+++ src/main/java/ru/netology/javaConfig/JavaConfig.java
@@ -0,0 +1,26 @@
+package ru.netology.javaConfig;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.sysProfile.DevProfile;
+import ru.netology.sysProfile.ProductionProfile;
+import ru.netology.sysProfile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+
+
+}
---
+++ src/main/java/ru/netology/sysProfile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.sysProfile;
+
+public interface SystemProfile {
+    String getProfile();
+}
